682a5302eeaf38355da7cf188c7e71838d8709cb
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-03-02 16:55:00 +0200

    added videoplay + simple router and vuex setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="home">
-    <h1>Vudeos</h1>
+    <h1>Videos</h1>
+    <div class="video-container">
+      <div v-for="(video, index) in videos" :key="index">
+        <router-link :to="{ name: 'video-watch', params: { id: video.id } }">
+          <div class="video-box">
+            <img :src="video.thumbnail" />
+            <div>
+              <h3>{{ video.name }}</h3>
+              <div v-html="video.description"></div>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,7 +24,27 @@ export default {
   name: "Home",
   components: {},
   data() {
-    return {};
+    return {
+      videos: this.$store.state.videos,
+    };
   },
 };
 </script>
+
+<style scoped lang="scss">
+.video-container {
+  .video-box {
+    border: 1px solid black;
+    border-radius: 10px;
+    margin: 10px;
+    padding: 10px;
+    text-align: left;
+    display: flex;
+    justify-content: flex-start;
+    img {
+      width: 200px;
+      padding: 10px;
+    }
+  }
+}
+</style>
