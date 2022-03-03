fc8ac77f2d4385083a65edf9553859e14585e17f
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-03-03 17:28:48 +0200

    bux fixes

---
+++ src/views/VideoWatch.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <video width="400" controls>
+      <source ref="videoRef" />
+      Your browser does not support HTML video.
+    </video>
+
+    <h1>{{ video.name }}</h1>
+    <div v-html="video.description"></div>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    video() {
+      return this.$store.state.videos.find(
+        (vid) => vid.id == this.$route.params.id
+      );
+    },
+  },
+  mounted: function () {
+    this.$refs.videoRef.src = this.video.videoUrl;
+  },
+};
+</script>
+
+<style scoped>
+img {
+  max-width: 50%;
+}
+</style>
